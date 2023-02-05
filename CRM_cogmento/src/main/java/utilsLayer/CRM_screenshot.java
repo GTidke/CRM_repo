5423c92a924dcff7e3533ff74a8240e0e7057b64
GTidke <gauravttidke@gmail.com>
2023-02-05 22:44:36 +0530

    Screenshot

--- CRM_cogmento/src/main/java/utilsLayer/CRM_screenshot.java
+++ CRM_cogmento/src/main/java/utilsLayer/CRM_screenshot.java
@@ -9,23 +9,21 @@ import org.openqa.selenium.TakesScreenshot;
 
 import baseLayer.CRM_baseclass;
 
-public class CRM_screenshot extends CRM_baseclass{
+public class CRM_screenshot extends CRM_baseclass {
 	File f;
+
 	public CRM_screenshot() {
-	TakesScreenshot ss=(TakesScreenshot)driver;
-	File f=ss.getScreenshotAs(OutputType.FILE);
+		TakesScreenshot ss = (TakesScreenshot) driver;
+		File f = ss.getScreenshotAs(OutputType.FILE);
 	}
-	
+
 	public void PassScreenshot() {
-		File dest=new File(System.getProperty("user.dir")+"passScreenShot/name.png");
-	   try {
-		FileUtils.copyFile(f, dest);
-	} catch (IOException e) {
-		e.printStackTrace();
+		File dest = new File(System.getProperty("user.dir") + "passScreenShot/name.png");
+		try {
+			FileUtils.copyFile(f, dest);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	}
-
-	
-
 
 }
