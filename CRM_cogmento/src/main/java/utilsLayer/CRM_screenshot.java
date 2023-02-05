65b5968e9ef1c86a5403067f910e965c51af4b02
GTidke <gauravttidke@gmail.com>
2023-02-05 22:43:28 +0530

    ScreenShot class added in utils package

---
+++ CRM_cogmento/src/main/java/utilsLayer/CRM_screenshot.java
@@ -0,0 +1,31 @@
+package utilsLayer;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import baseLayer.CRM_baseclass;
+
+public class CRM_screenshot extends CRM_baseclass{
+	File f;
+	public CRM_screenshot() {
+	TakesScreenshot ss=(TakesScreenshot)driver;
+	File f=ss.getScreenshotAs(OutputType.FILE);
+	}
+	
+	public void PassScreenshot() {
+		File dest=new File(System.getProperty("user.dir")+"passScreenShot/name.png");
+	   try {
+		FileUtils.copyFile(f, dest);
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	}
+
+	
+
+
+}
